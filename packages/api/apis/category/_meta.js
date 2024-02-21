ae96da66dc52393c9679201776b6716c3cf2336e
chensuiyi <chenbimo@gmail.com>
2024-02-21 22:50:36 +0800

    调整配置(api): 调整接口的元数据配置

--- packages/api/apis/category/_meta.js
+++ packages/api/apis/category/_meta.js
@@ -1,19 +1,14 @@
-import * as yiapi from '@yicode/yiapi';
+import { fnSchema, fnMeta } from '@yicode/yiapi/fn.js';
+import { schemaField } from '@yicode/yiapi/config/schemaField.js';
 
-export const metaConfig = {
+export const metaConfig = fnMeta(import.meta.url, {
     _name: '导航分类',
-    tableName: 'nav_category',
-    schema: {
-        id: yiapi.utils.fnSchema(yiapi.schemaField.id, '自增ID'),
-        page: yiapi.utils.fnSchema(yiapi.schemaField.page, '第几页'),
-        limit: yiapi.utils.fnSchema(yiapi.schemaField.limit, '每页数量'),
-        pid: yiapi.utils.fnSchema(yiapi.schemaField.pid, '父级分类ID'),
-        name: yiapi.utils.fnSchema(null, '分类名称', 'string', 1, 30),
-        icon: yiapi.utils.fnSchema(yiapi.schemaField.image, '分类图标'),
-        sort: yiapi.utils.fnSchema(yiapi.schemaField.min1, '分类排序'),
-        describe: yiapi.utils.fnSchema(yiapi.schemaField.describe, '分类描述'),
-        type: yiapi.utils.fnSchema(null, '数据类型', 'string', null, null, ['mine', 'default']),
-        mode: yiapi.utils.fnSchema(null, '数据模式', 'string', null, null, ['k', 's']),
-        level: yiapi.utils.fnSchema(null, '数据等级', 'string', null, null, ['mini', 'medium', 'large'])
-    }
-};
+    pid: fnSchema(schemaField.pid, '父级分类ID'),
+    name: fnSchema(null, '分类名称', 'string', 1, 30),
+    icon: fnSchema(schemaField.image, '分类图标'),
+    sort: fnSchema(schemaField.min1, '分类排序'),
+    describe: fnSchema(null, '分类描述', 'string', 0, 200, ''),
+    type: fnSchema(null, '数据类型', 'string', null, null, ['mine', 'default']),
+    mode: fnSchema(null, '数据模式', 'string', null, null, ['k', 's']),
+    level: fnSchema(null, '数据等级', 'string', null, null, ['mini', 'medium', 'large'])
+});
