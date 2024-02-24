dd1aedd7ab143d089b0687603513f47642b7d035
chensuiyi <chenbimo@gmail.com>
2024-02-24 20:58:32 +0800

    调整配置(api): 修改pm2运行名称为yinav

--- packages/api/pm2.config.cjs
+++ packages/api/pm2.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'yicode_demo',
+            name: 'yinav',
             instances: 'max',
             script: './yiapi.js',
             exec_mode: 'cluster',
