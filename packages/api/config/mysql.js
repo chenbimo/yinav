031e94132eed94a928989b41f77dcb309263b0f3
chensuiyi <chenbimo@gmail.com>
2024-02-24 20:56:46 +0800

    调整配置(api): 调整数据库连接配置

--- packages/api/config/mysql.js
+++ packages/api/config/mysql.js
@@ -1,18 +1,18 @@
 export const mysqlConfig = {
     // 开发环境配置
     development: {
-        host: '127.0.0.1',
-        port: 3306,
-        db: 'yinav',
-        username: 'root',
-        password: 'root'
+        // host: '127.0.0.1',
+        // port: 3306,
+        // db: 'yinav',
+        // username: 'root',
+        // password: 'root',
     },
     // 生产环境配置
     production: {
-        host: '127.0.0.1',
+        host: '10.0.4.6',
         port: 3306,
         db: 'yinav',
-        username: 'root',
-        password: 'root'
+        username: 'yinav',
+        password: 'yinav123456'
     }
 }[process.env.NODE_ENV];
