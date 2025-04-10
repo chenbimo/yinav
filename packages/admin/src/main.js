edfb96e144bd79a549a0508dc05e23f393ccdbae
陈随易 <chenbimo@gmail.com>
2025-04-10 12:31:53 +0800

    调整yite-cli打包配置

--- packages/admin/src/main.js
+++ packages/admin/src/main.js
@@ -1,16 +1,13 @@
 import App from '@/App.vue';
-import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import '@arco-design/web-vue/dist/arco.css';
 
 import 'virtual:uno.css';
 
 const app = createApp(App);
-
 const $Pinia = Pinia.createPinia();
 
 app.use($Router);
 app.use($Pinia);
 app.use($I18n);
-app.use(ArcoVueIcon);
 
 app.mount('#app');
