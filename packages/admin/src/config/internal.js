3d9de094af8c37770d667c37ac72fd846b672d26
陈随易 <chenbimo@gmail.com>
2025-04-17 15:21:44 +0800

    将获取用户token由cookie调整为localStorage。

--- packages/admin/src/config/internal.js
+++ packages/admin/src/config/internal.js
@@ -1,6 +1,6 @@
 export const $InternalConfig = {
     // 用户令牌
-    token: yite.getCookie('token') || '',
+    token: $Storage.local.get('token') || '',
     // 用户数据
     userData: $Storage.local.get('userData') || {},
     // 表格边框
@@ -10,8 +10,11 @@ export const $InternalConfig = {
     // 表格滚动
     tableScroll: {
         x: '100%',
-        y: '100%'
+        y: '100%',
+        maxHeight: '100%'
     },
+    modalShortWidth: 600,
+    modalLongWidth: 1200,
     // 抽屉默认宽度
     drawerWidth: 400,
     // 每页显示数量
