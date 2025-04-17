b1461c6433cc594c3b2b634ab12461c81ef99f41
陈随易 <chenbimo@gmail.com>
2025-04-17 15:15:07 +0800

    调整导航菜单

--- packages/api/config/menu.js
+++ packages/api/config/menu.js
@@ -1,19 +1,13 @@
 // 自定义菜单
 export const menuConfig = [
     {
-        name: '导航分类',
-        path: '/category',
+        name: '导航管理',
+        path: '/nav-manager',
         children: [
             {
                 path: '/category/list',
                 name: '分类列表'
-            }
-        ]
-    },
-    {
-        name: '导航管理',
-        path: '/navigation',
-        children: [
+            },
             {
                 name: '导航列表',
                 path: '/navigation/list'
