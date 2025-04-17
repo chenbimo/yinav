8a224f367eba24c67d604450c305da874feac275
陈随易 <chenbimo@gmail.com>
2025-04-17 15:25:08 +0800

    修复国际化支持legacy警告信息问题

--- packages/admin/src/plugins/i18n.js
+++ packages/admin/src/plugins/i18n.js
@@ -1,6 +1,8 @@
+import { createI18n } from 'vue-i18n';
 import { yiteMessages } from 'virtual:yite-messages';
 
 const $I18n = createI18n({
+    legacy: false,
     locale: 'zh',
     messages: await yiteMessages()
 });
